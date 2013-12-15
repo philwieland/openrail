2735923f59d3100fac406d2b460a31cff0ce5452
Phil Wieland <phil@philwieland.com>
2013-12-15 20:12:45 +0000

    Copyright stuff.

--- liverail.js
+++ liverail.js
@@ -1,3 +1,23 @@
+/*
+    Copyright (C) 2013 Phil Wieland
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    phil@philwieland.com
+
+*/
+
 var url_base = "/rail/liverail/";
 var refresh_tick = 4096; /* ms between ticks */
 //var refresh_tick = 400; /* Testing only */
