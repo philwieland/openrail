da7ae93d6cc607e79f4e9cfbd44eb8e8bb878f78
Phil Wieland <wielanpj@sausage.tarbock.local>
2022-12-23 18:55:00 +0000

    Release 3C22

--- build.h
+++ build.h
@@ -1,3 +1,6 @@
-// build.h Before release.
+// build.h created by release-tag Y123.
 
-//#define RELEASE_BUILD = "Unreleased"
+// Subversion tag "3C22", revision 2160.
+
+#define RELEASE_BUILD "3C22"
+
